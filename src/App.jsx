58ad39238155dd9db3e348504bd004a4e222c8dd
Prohalladdas <prolladdas@gmail.com>
2025-03-30 14:22:51 +0530

    Implementing Main Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Priceing from "./pages/Priceing";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+
 export default function App() {
-  return <div>hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Priceing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
