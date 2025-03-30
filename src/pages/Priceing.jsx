524615383d2e5e10403d189bd59a1d0477e15a5a
Prohalladdas <prolladdas@gmail.com>
2025-03-30 14:57:47 +0530

    Linking Between Routes With Link  and NavLink

--- src/pages/Priceing.jsx
+++ src/pages/Priceing.jsx
@@ -1,3 +1,10 @@
+import PageNav from "../components/PageNav";
+
 export default function Priceing() {
-  return <div>Pricing</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Pricing</h1>
+    </div>
+  );
 }
