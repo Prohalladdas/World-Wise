90802d756299b065686a85cc9749c8ab521122d6
Prohalladdas <prolladdas@gmail.com>
2025-04-01 21:49:01 +0530

    Building the Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
-import Priceing from "./pages/Priceing";
+import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
@@ -11,7 +12,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Priceing />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
