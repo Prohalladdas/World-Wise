dbb53006e6865ababa6ec3508a2112385603dc96
Prohalladdas <prolladdas@gmail.com>
2025-04-13 18:24:46 +0530

    Dynamic Routes With URL Parameters

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
 
 const BASE_URL = "http://localhost:9000";
 export default function App() {
@@ -42,7 +43,7 @@ export default function App() {
         <Route index element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
           <Route
             index
@@ -52,6 +53,9 @@ export default function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+
+          <Route path="cities/:id" element={<City />} />
+
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
